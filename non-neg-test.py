35e170b2ed458e9aa9347bbddcee079a40bb1137
limqiying <limqiying19@gmail.com>
2017-11-28 21:40:07 -0500

    moved correctnes method

--- non-neg-test.py
+++ non-neg-test.py
@@ -46,33 +46,5 @@ class NonNegativeTest:
             return "Index out of range"
         return self._random_graphs[index]
 
-    def correctness_test(self):
-        """
-        Runs test on all the randomly generated graphs.
-        If there is an error, this will be raised by the assertion.
-        Otherwise, a simple "test passed" will be created.
-        This test only checks that the distance computed by the brute force method is equal to ths distance computed
-        by Dijkstra's algorithm.
-        This avoids the problem where there might be two paths that have the same smallest distances.
-        """
-        test_num = 1  # counter for the number of tests
-        for graph in self._random_graphs:
-            try:
-                node = choice(list(graph.get_nodes()))  # randomly chooses some node in the node list
-            except IndexError:
-                print('graph has no nodes')
-            else:
-                (brute_path, brute_dist) = TestTools.brute_force_result(graph, node)
-                d_dist = graph.dijkstra_get_dist(node, numerical=True)
-                assert brute_dist == d_dist, "brute distance is " + str(brute_dist) + " while d computed " + str(d_dist)
-                print("Test " + str(test_num) + " passed.")
-                test_num += 1
-
-
-<<<<<<< HEAD
-test = NonNegativeTest(100)
-test.correctness_test()
-=======
-test = NonNegativeTest(1)
-g = test.get_graph()
->>>>>>> cca1e81b091c0811af1fde37fb2ed62a0c7662ed
+    def run_correctness(self):
+        TestTools.correctness_test(self._random_graphs)
